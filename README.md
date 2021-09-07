5f90e5813895632f1a2896e42deaad2e5965f6b3
anthonyboulay <air_walk_kid@hotmail.com>
2021-09-07 11:43:02 -0400

    Ready for download

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # work_timer_public
 A simple timer that will remind you to get away from your computer. Public Version
+
+install via target folder and run "Work TimerSetup.exe"
---
+++ target/PyInstaller/fbs_pyinstaller_hook.py
@@ -0,0 +1,3 @@
+import importlib
+module = importlib.import_module('fbs_runtime._frozen')
+module.BUILD_SETTINGS = {'app_name': 'Work Timer', 'author': 'anthonyboulay', 'version': '0.0.1', 'environment': 'local'}
